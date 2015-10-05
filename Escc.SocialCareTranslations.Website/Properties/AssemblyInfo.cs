a84294d6e8d51aa987e05670f9b6c54e8e1d0974
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-10-05 10:29:33 +0100

    Initial commit

---
+++ Escc.SocialCareTranslations.Website/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Escc.SocialCareTranslations.Website")]
+[assembly: AssemblyDescription("Details in multiple languages of how to get translated leaflets from Adult Social Care")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("East Sussex County Council")]
+[assembly: AssemblyProduct("Escc.SocialCareTranslations.Website")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("East Sussex County Council")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96cd85d0-65d9-4f20-a9db-721bcd2d5e79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Escc.SocialCareTranslations.Website
+
+Details in multiple languages of how to get translated leaflets from Adult Social Care.
+
+## Development setup steps
+
+1. From an Administrator command prompt, run `app-setup-dev.cmd` to set up a site in IIS.
+2. Build the solution
+3. In `~\web.config` uncomment and complete the `Proxy` section.
