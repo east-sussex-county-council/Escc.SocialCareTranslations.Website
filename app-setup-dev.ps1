5f471fa7b73e16a88f8e051466b2c016f558cde3
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-04-25 17:31:59 +0100

    Remove obsolete dependencies from solution

--- app-setup-dev.ps1
+++ app-setup-dev.ps1
@@ -39,11 +39,6 @@ $projectName = "Escc.SocialCareTranslations.Website"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.EastSussexGovUK"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.NavigationControls"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Data.Web"
-DownloadProjectIfMissing $parentFolderOfThisScript "Escc.HouseStyle"
-DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Data.ActiveDirectory"
-DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Cms"
-DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Elibrary"
-NuGetRestoreForProject $parentFolderOfThisScript "Escc.Cms"
 NuGetRestoreForProject $parentFolderOfThisScript "Escc.EastSussexGovUK"
 
 EnableDotNet40InIIS
